50e42de2ad13d63b21193ae013c3689306a80bc9
Alphin Kayalathu Mathew <62940162+alphin20@users.noreply.github.com>
2020-04-02 12:57:34 +0530

    Add files via upload

---
+++ c4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int petage= 4; char petname []= "luka"; char favfood []="pedigree,meat";
+  printf("Name of the student: %s\n",petname);
+  printf("Age of the student :%d\n ",petage );
+  printf("fav food :%s\n",favfood);
+  return 0;
+}
---
+++ code3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int age= 10; char name []= "Anamika"; float marks= 49.5;
+  printf("Name of the student %s\n",name);
+  printf("Age of the student %d\n ",age );
+  printf("marks obtained %f\n",marks);
+  return 0;
+}
