121f4b8ff5e9cef7099cd6389b6397c68895aa81
Alphin Kayalathu Mathew <62940162+alphin20@users.noreply.github.com>
2020-04-05 16:20:10 +0530

    Add files via upload

---
+++ days of the week.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{int x;
+  printf("Enter your Number!");
+  scanf("%d",&x);
+  switch(x)
+  {
+    case 1:printf("Monday");
+    break;
+    
+    case 2:printf("Tuesday");
+    break;
+    case 3:printf("Wednesday");
+    break;
+    case 4:printf("Thursday");
+    break;
+    case 5:printf("Friday");
+    break;
+    case 6:printf("Saturday");
+    break;
+    case 7:printf("Sunday");
+    break;
+    default:printf("default"); 
+    break;
+  }
+  return 0;
+}
